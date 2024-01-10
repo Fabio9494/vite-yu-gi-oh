64e4fa31ca569052ee6c59c75ef6cd5d33e6ebed
Fabio9494 <fabio.casoria94@gmail.com>
2024-01-10 19:54:27 +0100

    add header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template lang="">
+    <header>
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    background-color: white;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    padding: 10px;
+    border: solid black 2px;
+}
+</style>
